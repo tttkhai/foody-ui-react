4b192187a7c82b7f91866c669bcbfdb18591e1e2
Khai Tran <tttkhai93@gmail.com>
2021-01-18 23:56:33 -0500

    configured proxy to point to server port

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,5 +1,4 @@
 export const endpoints= {
-    server: "http://localhost:8081/api/",
-    login: "http://localhost:8081/api/login",
-    currentUser: "http://localhost:8081/api/currentUser"
+    login: "/api/login",
+    currentUser: "/api/currentUser"
 }
